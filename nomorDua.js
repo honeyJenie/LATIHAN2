dc8844ad2b4608ddd4fa75a000e12df6e1c3f069
Jenie Selina <jenie.selina@gmail.com>
2023-01-18 13:32:46 +0700

    code yg sudah jadi

--- nomorDua.js
+++ nomorDua.js
@@ -1,1 +1,29 @@
-//test2
+//Program untuk mengubah urutan kata pada sebuah kalimat
+
+const revSentence = (b)=>{
+    if(typeof b !== "string"){
+        return "Kalimat harus berupa string"
+    }else{
+        let temp = [""]
+        let result = ""
+        let k = 0
+        for (let i = 0; i < b.length; i++){
+            if (b[i] !== " "){
+                temp[k] += b[i];
+            }else{
+                k += 1
+                temp[k] = ""
+            }
+        }
+        for (let j = temp.length-1; j >= 0; j--){
+            if (j === 0){
+                result += temp[j]
+            } else{
+                result += temp[j] + " "
+            }
+        }
+        return result
+    }
+}
+
+console.log(revSentence("Jodie paling ganteng sejagat raya"))
--- nomorSatu.js
+++ nomorSatu.js
@@ -1,2 +1,29 @@
-//test1
-console.log("jodie aye")
+// program untuk memeriksa apakah sebuah teks merupakan palindrom atau bukan
+
+const cekPalindrom = (a)=>{
+    if(typeof a !== "string"){
+        return "Teks harus string."
+    } else{
+        if(a.length % 2 === 0){
+            for (let i = 0; i < a.length / 2; i++){
+                let k = a.length - 1 - i
+                if (a[i] !== a[k]){
+                    return `${a} = Bukan Palindrom.`
+                }else{
+                    return `${a} = Palindrom.`
+                };
+            };
+        } else{
+            for (let i = 0; i < (a.length - 1) / 2; i++){
+                let k = a.length - 1 - i
+                if (a[i] !== a[k]){
+                    return `${a} = Bukan Palindrom.`
+                }else{
+                    return `${a} = Palindrom.`
+                };
+            };
+        };
+    };
+}
+
+console.log(cekPalindrom("jenieeniej"))
